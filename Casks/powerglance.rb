6aafa15be7a7e5ff3c27a496573ca2826b64ba97
i2 <95607264+YosukeIida@users.noreply.github.com>
2026-07-08 23:29:51 +0900

    feat: update powerglance to 0.2.1

--- Casks/powerglance.rb
+++ Casks/powerglance.rb
@@ -1,6 +1,6 @@
 cask "powerglance" do
-  version "0.2.0"
-  sha256 :no_check
+  version "0.2.1"
+  sha256 "96da1d6549de6ebcf6fd6701046b5ee5c358c0efe6a2d920de4c8854db22baeb"
 
   url "https://github.com/YosukeIida/PowerGlance/releases/download/v#{version}/PowerGlance-#{version}.dmg"
   name "PowerGlance"
